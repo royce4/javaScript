6edf1b90ab229387a00a9831df4402368865dc7a
royce4 <william.royce@hotmail.com>
2018-10-17 17:09:02 -0500

    Initial commit

---
+++ reaction_time_script.js
@@ -0,0 +1,49 @@
+            var start = new Date().getTime();
+            var shapes = ["0%","50%"];
+            appearAfterDelay();
+
+//Create a random hex number for color styling
+            function createHex(){
+                var hexString = "#";
+                var number, hex;
+                for (var i =1;i<4;i++){
+                    number = Math.floor(Math.random()*255);
+                    hex = number.toString(16);
+                    hexString += hex.toString();    
+                }
+               
+                return(hexString);
+
+            }
+// Make somewhat random shape appear, either square or circle. With stylization of size, color and location randomized
+// Start timer after creation 
+            function makeShapeAppear(){
+                var top = Math.random()*400;
+                var left = Math.random()*400;
+                var width = (Math.random()*300)+100;
+                document.getElementById("shape").style.display = "block";
+                document.getElementById("shape").style.top = top + "px";
+                document.getElementById("shape").style.left = left + "px";
+                document.getElementById("shape").style.width = width + "px";
+                document.getElementById("shape").style.height = width + "px";
+                document.getElementById("shape").style.background = createHex();
+                document.getElementById("shape").style.borderRadius = shapes[Math.floor(Math.random()*2)];
+                start = new Date().getTime();
+               
+
+            }
+// Waiting period to display the shape
+            function appearAfterDelay(){
+                setTimeout(makeShapeAppear(), (Math.random()*2000));
+
+            }
+// Main function. Once the shape is clicked it disappears and reaction time is calculated and displayed.
+
+            document.getElementById("shape").onclick = function(){
+                document.getElementById("shape").style.display = "none";
+                var end = new Date().getTime();
+                var timeTaken = (end-start) / 1000;
+                document.getElementById("timer").innerHTML = timeTaken + "s";
+                appearAfterDelay();
+
+            }
